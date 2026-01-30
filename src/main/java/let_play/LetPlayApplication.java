78e718522b07790db2d798c557af5e20080e34e1
sbeytour <beytour.safae@gmail.com>
2026-01-30 14:57:22 +0100

    setting up database

--- src/main/java/let_play/LetPlayApplication.java
+++ src/main/java/let_play/LetPlayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LetPalyApplication {
+public class LetPlayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LetPalyApplication.class, args);
+		SpringApplication.run(LetPlayApplication.class, args);
 	}
 
 }
