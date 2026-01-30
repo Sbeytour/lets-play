6ba2ea653b7ea697bce411e0c836587a37b1c7be
sbeytour <beytour.safae@gmail.com>
2026-01-30 11:09:17 +0100

    first commit/ set uping project

---
+++ src/main/java/let_play/LetPalyApplication.java
@@ -0,0 +1,13 @@
+package let_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetPalyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetPalyApplication.class, args);
+	}
+
+}
