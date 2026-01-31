4aff90bcad0b1649fd1aedb8801ef010ce253a6a
sbeytour <beytour.safae@gmail.com>
2026-01-31 11:01:03 +0100

    add jwt Utils

---
+++ src/main/java/let_play/security/jwtUtils.java
@@ -0,0 +1,106 @@
+package let_play.security;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import let_play.entities.User;
+
+public class jwtUtils {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    public String generateToken(User user) {
+        try {
+            String email = user.getEmail();
+            Date now = new Date();
+            Date expaireDate = new Date(now.getTime() + jwtExpiration);
+
+            return Jwts.builder().setSubject(email).setIssuedAt(now).setExpiration(expaireDate)
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                    .compact();
+        } catch (Exception e) {
+            return null; // handle exceptions
+        }
+    }
+
+    private Key getSignInKey() {
+        try {
+            byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+            return Keys.hmacShaKeyFor(keyBytes);
+        } catch (Exception e) {
+            return null; // handle exceptions
+        }
+    }
+
+    public String extractUserEmail(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
+            return claims.getSubject();
+        } catch (Exception e) {
+            return null; // handle exceptions
+        }
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            if (token == null || token.trim().isEmpty()) {
+                return false;
+            }
+
+            return !isTokenExpired(token);
+
+        } catch (ExpiredJwtException e) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        try {
+            return extractExpiration(token).before(new Date());
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
+        } catch (SecurityException e) {
+            return null; // handle exception
+        } catch (MalformedJwtException e) {
+            return null; // handle exception
+        } catch (ExpiredJwtException e) {
+            throw e;
+        } catch (UnsupportedJwtException e) {
+            return null; // handle exception
+        } catch (IllegalArgumentException e) {
+            return null; // handle exception
+        }
+    }
+
+}
